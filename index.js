d2b61b791a550dc7d879b60080ec9d156bc1e38a
madhurrajput23 <105670693+madhurrajput23@users.noreply.github.com>
2023-04-16 00:16:56 +0530

    complete crud operation

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const addNoteBtn = document.querySelector(".addbtn");
 
+const updateLocalSData = () =>{
+    const textAreaData = document.querySelectorAll('textarea');
+    const noteEmptyArr = [];
+      
+    textAreaData.forEach((currElem) => {
+        return noteEmptyArr.push(currElem.value);
+       
+    })
+    localStorage.setItem('notes' ,JSON.stringify(noteEmptyArr))
+}
+
 const addNoteBox = (text = "") => {
   const note = document.createElement("div");
   note.classList.add("note");
@@ -23,6 +34,7 @@ const addNoteBox = (text = "") => {
 
   deleteBtn.addEventListener("click", () => {
     note.remove();
+    updateLocalSData();
   });
 
   editBtn.addEventListener("click", () => {
@@ -30,7 +42,22 @@ const addNoteBox = (text = "") => {
     textArea.classList.toggle('hidden')
   })
 
+  textArea.addEventListener('change' , (event) => {
+    const value = event.target.value;
+    mainDiv.innerHTML = value;
+
+    updateLocalSData();
+  })
+
   document.body.appendChild(note);
 };
+
+//getting data back from storage
+
+const notesData = JSON.parse(localStorage.getItem('notes'));
+ 
+if(notesData){
+    notesData.forEach((note) => addNoteBox(note));
+}
 
 addNoteBtn.addEventListener("click", () => addNoteBox());
